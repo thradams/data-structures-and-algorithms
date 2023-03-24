505c1ebae378ce1a070919ece73908936acd264b
thradams <thiago@elipse.com.br>
2023-03-24 11:19:26 -0300

    try catch macros removed

--- generator/preprocessor.js
+++ generator/preprocessor.js
@@ -1,33 +1,52 @@
 
-function preprocessor(text) {
+function preprocessor(text)
+{
     var out = "";
     var lines = text.split("\n");
-    for (var i = 0; i < lines.length; i++) {
+    for (var i = 0; i < lines.length; i++)
+    {
         var line = lines[i];
 
         var k = 0;
         while (line.charAt(k) == " ") k++;
 
-        if (line.charAt(k) == "@") {
+        if (line.charAt(k) == "@")
+        {
             k++;
             k++;//@(
+            var allblanks = true;
             var start = k;
-            while (true) {
-                if (line.charAt(k) == ')') {
+            while (true)
+            {
+                if (line.charAt(k) == ')')
+                {
                     k++;
                     break;
                 }
                 else
+                {
+                    if (line.charAt(k) != ' ') allblanks = false;
                     k++;
+                }
             }
             var expression = line.substring(start, k - 1);
-            var result = eval(expression);
+            var result = false;
+            if (!allblanks)
+            {
+                result = eval(expression);
+            }
+            else
+            {
+                result = true;
+            }
 
-            if (result) {
+            if (result)
+            {
                 out += line.substring(k, line.length) + "\n";
             }
         }
-        else {
+        else
+        {
             out += line + "\n";
         }
     }
